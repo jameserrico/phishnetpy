3a14367a8f2734d746f365c672acd0c6803edb17
James Errico <james.errico@gmail.com>
2015-09-13 13:43:05 -0400

    Updated Readme

---
+++ setup.py
@@ -0,0 +1,24 @@
+__author__ = 'jerrico'
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+from phishnetpy import VERSION
+
+setup(name='phishnetpy',
+      version=VERSION,
+      description='Python client for the Phish.net API',
+      author='James Errico',
+      author_email='james.errico@gmail.com',
+      url='https://github.com/jameserrico/phishnetpy',
+      py_modules=['phisnetpy'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+      ],
+)
