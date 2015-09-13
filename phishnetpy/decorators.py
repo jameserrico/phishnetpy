83d6a5fe4042bee5ea0939efd05d15691dc4cbd4
James Errico <james.errico@gmail.com>
2015-09-13 10:38:52 -0400

    Using qualname package on python2 since __qualname__ does not yet exist

--- phishnetpy/decorators.py
+++ phishnetpy/decorators.py
@@ -2,21 +2,28 @@ __author__ = 'jerrico'
 
 from phishnetpy.exceptions import AuthError
 
+def qual_name_safe(f):
+    try:
+        return f.__qualname__
+    except AttributeError:  # Occurs when pyhon <= 3.3
+        import qualname
+        return qualname(f)
+
 
 def check_api_key(f):
     def wrapper(*args, **kwargs):
         if not args[0].api_key:
-            raise AuthError("{} requires an API key".format(f.__qualname__))
+            raise AuthError("{} requires an API key".format(qual_name_safe(f)))
         return f(*args, **kwargs)
     return wrapper
 
 def check_authorized_user(f):
     def wrapper(*args, **kwargs):
         if not args[0].api_key:
-            raise AuthError("{} requires an API key".format(f.__qualname__))
+            raise AuthError("{} requires an API key".format(qual_name_safe(f)))
         if not args[0].username:
-            raise AuthError("{} requires an authorized username".format(f.__qualname__))
+            raise AuthError("{} requires an authorized username".format(qual_name_safe(f)))
         if not args[0].auth_key:
-            raise AuthError("{} requires an authkey".format(f.__qualname__))
+            raise AuthError("{} requires an authkey".format(qual_name_safe(f)))
         return f(*args, **kwargs)
     return wrapper
