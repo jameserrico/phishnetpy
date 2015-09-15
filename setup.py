6f87e923d00da5dca52f1d3602f07873b2973571
James Errico <james.errico@gmail.com>
2015-09-15 11:11:21 -0400

    Pip install instructions

--- setup.py
+++ setup.py
@@ -4,28 +4,29 @@ from distutils.core import setup
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 install_reqs = parse_requirements('requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
-
-setup(name='phishnetpy',
-      version=VERSION,
-      description='Python client for the Phish.net API',
-      author='James Errico',
-      author_email='james.errico@gmail.com',
-      url='https://github.com/jameserrico/phishnetpy',
-      packages=['phishnetpy'],
-      install_requires=reqs,
-      license='MIT',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.4',
-      ],
+setup(
+    name='phishnetpy',
+    version=VERSION,
+    description='Python client for the Phish.net API',
+    author='James Errico',
+    author_email='james.errico@gmail.com',
+    url='https://github.com/jameserrico/phishnetpy',
+    download_url='https://github.com/jameserrico/phishnetpy/tarball/{}'.format(VERSION),
+    packages=['phishnetpy'],
+    install_requires=reqs,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
