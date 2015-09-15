c236312f0d36af9a790a7f6344d23a7f5d60fb96
James Errico <james.errico@gmail.com>
2015-09-15 11:08:55 -0400

    Pip install instructions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 install_reqs = parse_requirements('requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
