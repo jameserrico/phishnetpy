23a6a95cb1e66fbe222eaf5e636d103cdbdd2997
Matt Garbis <mgarbis@gmail.com>
2015-09-22 21:39:09 -0400

    setuptools tried to import main module before it was installed

--- phishnetpy/__init__.py
+++ phishnetpy/__init__.py
@@ -4,7 +4,8 @@ from phishnetpy.phishnet_api import PhishNetAPI
 from phishnetpy import decorators
 from phishnetpy import exceptions
 
-VERSION = '0.2.3'
+# this package is part of setuptools
+import pkg_resources
 
-__version__ = VERSION
-
+# the version is stored here upon initial installation
+__version__ = pkg_resources.require("phishnetpy")[0].version
--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ here = path.abspath(path.dirname(__file__))
 # with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 #     long_description = f.read()
 
-from phishnetpy import VERSION
-
 setup(
 
     name='phishnetpy',
@@ -19,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=VERSION,
+    version='0.2.4',
 
     description='Python client for the Phish.net API',
     # long_description=long_description,
