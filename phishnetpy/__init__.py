3d7ca501b9d8664b425208ddc34a554e66ad7009
Matt Garbis <mgarbis@gmail.com>
2015-09-22 21:55:21 -0400

    fixing build to cover local development condition

--- phishnetpy/__init__.py
+++ phishnetpy/__init__.py
@@ -8,4 +8,12 @@ from phishnetpy import exceptions
 import pkg_resources
 
 # the version is stored here upon initial installation
-__version__ = pkg_resources.require("phishnetpy")[0].version
+try:
+    __version__ = pkg_resources.require("phishnetpy")[0].version
+except pkg_resources.DistributionNotFound:
+    # this will happen during local development
+    # we should move the string to a TBD singular location
+    __version__ = '0.2.4'
+except:
+    print('could not determine version!')
+    raise
